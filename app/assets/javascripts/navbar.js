43c30a6a331af44d5971bb01af7d46a0cb13526b
adamjgoodman <goodmanadamj@gmail.com>
2020-04-23 21:53:46 -0400

    initial

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".dropdown-toggle, .btn-group").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});
+
