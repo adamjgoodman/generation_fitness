7daa40d77d5abe0cc7d91f8bac70b8ae3ec665e7
adamjgoodman <goodmanadamj@gmail.com>
2020-04-29 22:40:35 -0400

    change domain

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,10 @@
 class NotificationMailer < ApplicationMailer
-  default from: "memberservices@generationfitness.net"
+  default from: "memberservices@mygenerationfitness.com"
 
   def contact_form(prospect)
     @prospect = prospect
     mail(to: @prospect.email,
-    from: 'Member Services <memberservices@generationfitness.net>', 
+    from: 'Member Services <memberservices@mygenerationfitness.com>', 
     subject: "Thank you for contacting Generation Fitness",
     )
   end
